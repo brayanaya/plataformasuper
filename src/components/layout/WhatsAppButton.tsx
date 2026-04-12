2ec292c213e81321dc5d55a26523b6ed8d058eee
Brayan Aya <ayasteven599@gmail.com>
2026-04-11 22:18:19 -0500

    whatsapp button fix

--- src/components/layout/WhatsAppButton.tsx
+++ src/components/layout/WhatsAppButton.tsx
@@ -1,11 +1,6 @@
 export default function WhatsAppButton() {
   return (
-    
-      href="https://wa.me/573226937375"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 z-50 rounded-full w-14 h-14 flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-110"
-    >
+    <a href="https://wa.me/573226937375" target="_blank" rel="noopener noreferrer" className="fixed bottom-6 right-6 z-50 rounded-full w-14 h-14 flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-110">
       <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" className="w-14 h-14" />
     </a>
   )
